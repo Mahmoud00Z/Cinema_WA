aba770d7a51cb367569f207873896bb52d584193
Mahmoud00Z <zahabimahmoud@gmail.com>
2025-06-30 00:44:41 +0300

    created table showtimes

--- Cinema_server/migrations/003_create_showtimes_table.php
+++ Cinema_server/migrations/003_create_showtimes_table.php
@@ -1,3 +1,14 @@
 <?php
 
-require_once(__DIR__ . '/Cinema_server/connection/connection.php');
+require_once(__DIR__ . '/../connection/connection.php');
+
+$query = "CREATE TABLE IF NOT EXISTS showtimes (
+id INT AUTO_INCREMENT PRIMARY KEY,
+movie_id INT NOT NULL,
+show_datetime DATETIME NOT NULL,
+duration INT NOT NULL,
+FOREIGN KEY (movie_id) references movies(id)
+);
+";
+
+$mysqli->query($query);
--- Cinema_server/migrations/006_create_paymenst_table.php
+++ Cinema_server/migrations/006_create_paymenst_table.php
@@ -1,3 +1,3 @@
 <?php
 
-require_once(__DIR__ . '/Cinema_server/connection/connection.php');
+require_once(__DIR__ . '/../connection/connection.php');
