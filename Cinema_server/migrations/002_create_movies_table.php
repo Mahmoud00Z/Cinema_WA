57c63ac1429a059642cf7b37b48e9a45e1ba55a7
Mahmoud00Z <zahabimahmoud@gmail.com>
2025-06-30 00:26:24 +0300

    Created table movies

--- Cinema_server/migrations/002_create_movies_table.php
+++ Cinema_server/migrations/002_create_movies_table.php
@@ -1,3 +1,15 @@
 <?php
 
 require_once(__DIR__ . '/../connection/connection.php');
+
+$query = "CREATE TABLE IF NOT EXISTS movies (
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(255) NOT NULL,
+description TEXT NOT NULL,
+release_date DATE NOT NULL,
+cast JSON,
+rating TINYINT NOT NULL
+);
+";
+
+$mysqli->query($query);
