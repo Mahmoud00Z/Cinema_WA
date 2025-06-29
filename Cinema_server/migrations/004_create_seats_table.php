eb79e08c7206cb67859b79dbbcba46a1f3d0387b
Mahmoud00Z <zahabimahmoud@gmail.com>
2025-06-30 00:59:12 +0300

    created table seats

--- Cinema_server/migrations/004_create_seats_table.php
+++ Cinema_server/migrations/004_create_seats_table.php
@@ -1,3 +1,12 @@
 <?php
 
 require_once(__DIR__ . '/../connection/connection.php');
+
+$query = "CREATE TABLE IF NOT EXISTS seats (
+id INT AUTO_INCREMENT PRIMARY KEY,
+showtime_id INT NOT NULL,
+FOREIGN KEY (showtime_id) REFERENCES showtimes(id)
+);
+";
+
+$mysqli->query($query);
